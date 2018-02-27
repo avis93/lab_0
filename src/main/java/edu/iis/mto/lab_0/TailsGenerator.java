8852d629bda16e4e200b17debac180ce32ccef29
Famulski.Michal <187684@edu.p.lodz.pl>
2018-02-27 18:03:57 +0100

    Uzycie nowego formatera

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class TailsGenerator {
 
-	public List<String> tails(String value) {
-		List<String> list = new ArrayList<>(value.length() + 1);
-		for (int i = 0; i <= value.length(); i++) {
-			list.add(value.substring(i, value.length()));
-		}
-		return list;
-	}
+    public List<String> tails(String value) {
+        List<String> list = new ArrayList<>(value.length() + 1);
+        for (int i = 0; i <= value.length(); i++) {
+            list.add(value.substring(i, value.length()));
+        }
+        return list;
+    }
 
 }

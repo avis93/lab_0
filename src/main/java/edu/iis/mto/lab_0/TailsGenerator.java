ba6f41284a0795fd1e3d927311edeaca0839f618
pabich.wiktor <202258@edu.p.lodz.pl>
2018-02-27 17:38:55 +0100

    Fixed tails generating

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -1,12 +1,19 @@
 package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
-    }
 
+        ArrayList<String> tails = new ArrayList<String>();
+        tails.add(value);
+        for (int i = 1; i < value.length(); i++)
+            tails.add(tails.get(i-1).substring(1));
+
+        tails.add(tails.get(value.length() - 1).substring(1));
+        return tails;
+    }
 }

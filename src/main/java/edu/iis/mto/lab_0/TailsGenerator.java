8694d6d3ffe5d4fe78f0f09321566d1bcf0f6ece
gronek.grzegorz <202154@edu.p.lodz.pl>
2018-02-27 17:55:54 +0100

    fixed TailsGenerators so it returns proper substrings

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,15 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < value.length(); i++) {
+
+            result.add(value.substring(i, value.length()));
+        }
+
+        result.add("");
+        return result;
     }
 
 }

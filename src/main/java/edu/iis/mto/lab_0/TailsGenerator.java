28aa01f6ca9ba79d14b0f0e6e4afb308234b08a6
mjakmiyo <jedrzejekmonika@gmail.com>
2018-02-27 17:57:16 +0100

    Implement TailsGenerator

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,11 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
-    }
 
+        ArrayList<String> tailsList = new ArrayList<>();
+        for (int i = 0; i <= value.length(); i++) {
+            tailsList.add(value.substring(i));
+        }
+        return tailsList;
+    }
 }
